67a91e015c684646d9a658591ad89367f672a654
oliullah05 <mdolihasan@gmail.com>
2023-11-26 23:47:47 +0600

    mongoose connect with env done

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,34 @@
-import express from "express"
-export  const app = express()
+import express, { Application, Request, Response } from "express"
+export  const app:Application = express()
+import cors from "cors"
+
+// parsers
+app.use(express.json())
+app.use(cors())
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//middlewars
+const userRoute = express.Router()
+
+
+app.get('/', (req:Request, res:Response) => {
+  res.status(200).json({
+    success:true,
+    message:"welcome to Tours And Travels server"
+  })
 })
 
+app.use("/api/v1/users",userRoute)
+userRoute.get("/all-user",(req,res)=>{
+  const users = [
+    {id:1,
+    name:"laskdjs"},
+    {id:1,
+    name:"laskdjs"}
+  ]
+  res.status(200).json({
+    success:true,
+    data:users
+  })
+})
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import { app } from "./app"
 import  mongoose from 'mongoose';
+import config from "./config";
 
-const port = 3000
 
 
 
 // getting-started.js
 async function server() {
 try{
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
-  console.log("mongodb");
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  await mongoose.connect(config.database_url);
+
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`)
   })
 }
 catch(err){
