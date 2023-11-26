e8e07cda9d607602fcc21b8079ae226fe2eb3aa3
oliullah05 <mdolihasan@gmail.com>
2023-11-27 03:11:56 +0600

    basic crud oparation done

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,7 +4,10 @@ import { userController } from "../controllers/user.controller";
 
 const userRoutes = express.Router();
 userRoutes.post("/create-user",userController.createUser)
-
+userRoutes.get("/",userController.getAllUser)
+userRoutes.get("/:id",userController.getSingleUser)
+userRoutes.put("/:id",userController.updateUser)
+userRoutes.delete("/:id",userController.deleteUser)
 
 
 
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,5 +5,40 @@ const createUser = async(userData:IUser):Promise<IUser>=>{
     const result =await UserModel.create(userData)
     return result;
 }
+const getAllUser = async ():Promise<IUser[]>=>{
+    const result = await UserModel.find({
+        userStatus:"active"
+    });
+    return result
+}
+const getSingleUser =async(id:string):Promise<IUser|null>=>{
+const result = await UserModel.findById(id)
+return result
+}
 
-export const userServices ={createUser}
+
+const updateUser =async(id:string,userData:IUser):Promise<IUser|null>=>{
+    
+    const result = await UserModel.findByIdAndUpdate(id,userData,{
+        new:true,
+        runValidators:true
+    })
+    return result
+}
+
+
+
+const deleteUser =async(id:string):Promise<IUser|null>=>{
+    const result = await UserModel.findByIdAndDelete(id)
+    return result
+}
+
+
+
+export const userServices ={
+    createUser,
+    getAllUser,
+    getSingleUser,
+    updateUser,
+    deleteUser
+}
